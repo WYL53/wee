a4336caf6a9790827df4c4918a28c67f3cf756c8
843794313@qq.com <wuyl>
2020-06-04 10:16:56 +0800

    添加context

--- main.go
+++ main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	
+
 	"wee/wee"
 )
 
 func main() {
 	r := wee.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	r.GET("/", func(c *wee.Context) {
+		c.HTML(http.StatusOK, "<h1>Hello wee</h1>")
+	})
+	r.GET("/hello", func(c *wee.Context) {
+		// expect /hello?name=geektutu
+		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
 	})
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.POST("/login", func(c *wee.Context) {
+		c.JSON(http.StatusOK, wee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
